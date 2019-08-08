a7b484d96ac96cb2fe07c6185dbaf9bda898386d
zealshen <zealshen@outlook.com>
2019-08-08 14:29:11 +0100

    Try different feature selection methods on CorrAL dataset.

--- CorrAL/CorrAL_1.py
+++ CorrAL/CorrAL_1.py
@@ -1,3 +1,29 @@
 import numpy as np
 import pandas as pd
-hahahhaahha
+
+# load the datasets
+df = pd.read_csv('../Datasets/CorrAL.csv')
+
+array = df.values
+X = array[:, 0:6]
+y = array[:, 6]
+n_samples, n_features = X.shape
+
+## All data has been used to rank the features
+
+## Chi Square
+from skfeature.function.statistical_based import chi_square
+score_Chi = chi_square.chi_square(X, y)
+print('Chi Square score:', score_Chi)
+
+from skfeature.function.similarity_based import reliefF
+score_Rel = reliefF.reliefF(X, y)
+print('ReliefF score:', score_Rel)
+
+from skfeature.function.statistical_based import f_score
+score_F = f_score.f_score(X, y)
+print('F-score:', score_F)
+
+from skfeature.function.statistical_based import t_score
+score_T = t_score.t_score(X, y)
+print('t-score:', score_T)
